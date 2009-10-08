a4aad8f22735689b56361d65662f3f254c26f987
joaobispo <joaobispo@35153e2c-a92b-11de-80ce-d38665a2569c>
2009-10-08 14:28:34 +0000

    - Version 1 using GridBagLayout

--- src/JavaDriver/pt/ualg/Car/JavaDriver/GUI/DriverScreen.java
+++ src/JavaDriver/pt/ualg/Car/JavaDriver/GUI/DriverScreen.java
@@ -17,10 +17,11 @@
 
 package pt.ualg.Car.JavaDriver.GUI;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -54,20 +55,12 @@ public class DriverScreen {
 
       JPanel connectPanel = buildConnectPanel();
 
-      // Setup Frame
-      GridBagLayout gridBag = new GridBagLayout();
-      GridBagConstraints bagCons = new GridBagConstraints();
+      JPanel configPanel = buildConfigPanel();
 
-      //Insets insets = new Insets(20, 20, 20, 20);
-      //bagCons.insets = insets;
-      bagCons.weightx = 10.0;
+      buildJFrameBagLayout(configPanel);
+      //buildJFrameGridLayout(inputsPanel, connectPanel);
 
-      gridBag.setConstraints(windowFrame, bagCons);
-      
-      windowFrame.setLayout(gridBag);
-      windowFrame.add(inputsPanel);
-      windowFrame.add(connectPanel);
-      
+      windowFrame.setTitle("Carpad Driver");
       windowFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
       windowFrame.pack();
    }
@@ -82,19 +75,21 @@ public class DriverScreen {
 
       JavaDriverInput[] portNames = JavaDriverInput.values();
       for (int i = 0; i < numInputs; i++) {
-         // Create JTextFields
-         inputTextFields[i] = new JTextField();
-         // Setup JTextFields
-         inputTextFields[i].setEditable(false);
-         inputsPanel.add(inputTextFields[i]);
 
          // Create JLabels
          inputLabels[i] = new JLabel();
          // Setup JLabels
          String portName = portNames[i].getName();
-         inputLabels[i].setText(" " + portName);
+         inputLabels[i].setText(portName+":");
+         inputLabels[i].setHorizontalAlignment(JTextField.LEFT);
          inputsPanel.add(inputLabels[i]);
 
+         // Create JTextFields
+         inputTextFields[i] = new JTextField(5);
+         // Setup JTextFields
+         inputTextFields[i].setEditable(false);
+         inputTextFields[i].setHorizontalAlignment(JTextField.LEFT);
+         inputsPanel.add(inputTextFields[i]);
 
       }
 
@@ -104,20 +99,117 @@ public class DriverScreen {
 
    private JPanel buildConnectPanel() {
      JPanel connectPanel = new JPanel();
-     connectPanel.setLayout(new GridLayout(2, 1));
+     GridLayout gridLayout = new GridLayout(2, 1);
+     gridLayout.setVgap(10);
+     connectPanel.setLayout(gridLayout);
 
      connectButton = new JButton("Connect");
      connectButton.setEnabled(false);
      connectPanel.add(connectButton);
 
-     connectionStatus = new JTextField("Initializing...");
+     connectionStatus = new JTextField("Initializing...", 20);
      connectionStatus.setEditable(false);
      connectionStatus.setHorizontalAlignment(JTextField.CENTER);
+     connectionStatus.setMinimumSize(new Dimension(100, 40));
      connectPanel.add(connectionStatus);
 
      return connectPanel;
    }
 
+   private JPanel buildConfigPanel() {
+      //... Create an independent GridLayout panel of buttons.
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(2, 1, GAP, GAP));
+
+        calibrateButton = new JButton("Calibrate");
+        buttonPanel.add(calibrateButton);
+        configButton = new JButton("Redefine Keys");
+        buttonPanel.add(configButton);
+
+
+        //connectButton = new JButton("Connect");
+        //connectButton.setEnabled(false);
+        //buttonPanel.add(connectButton);
+
+        return buttonPanel;
+   }
+
+   private JPanel buildJFrameBagLayout(JPanel configPanel) {
+      //... Create GridBagLayout content pane; set border.
+      JPanel content = new JPanel(new GridBagLayout());
+      content.setBorder(BorderFactory.createEmptyBorder(BORDER, BORDER,
+              BORDER, BORDER));
+
+      GridBagHelper pos = new GridBagHelper();  // Create GridBag helper object.
+
+      // First Line
+      content.add(inputLabels[0], pos);
+      content.add(new Gap(GAP), pos.nextCol()); // 2nd col
+      content.add(inputTextFields[0], pos.nextCol()); // 3rd col
+
+      content.add(new Gap(GAP), pos.nextCol()); // 4th col
+      content.add(configPanel, pos.nextCol().height(5).align(GridBagConstraints.NORTH)); // 5th col
+
+      //content.add(new Gap(GAP) , pos.nextRow());  // Add a gap below
+      content.add(new Gap(GAP)  , pos.nextRow());
+      // Second line
+      content.add(inputLabels[1], pos.nextRow());
+      content.add(new Gap(GAP), pos.nextCol()); // 2nd col
+      content.add(inputTextFields[1], pos.nextCol()); // 3rd col
+
+      
+
+      /*
+      for (int i = 0; i < numInputs; i++) {
+         content.add(inputLabels[i], pos);
+         content.add(new Gap(GAP), pos.nextCol());
+         //content.add(inputTextFields[i], pos.nextCol().expandW());
+         content.add(inputTextFields[i], pos.nextCol());
+
+         if (i == 0) {
+            content.add(new Gap(GAP), pos.nextCol());
+            content.add(configPanel, pos.nextCol().height(5).align(GridBagConstraints.NORTH));
+         }
+
+         content.add(new Gap(GAP), pos.nextRow());  // Add a gap below
+         pos.nextRow();
+      }
+      */
+
+      //content.add(new Gap(GAP), pos.nextRow());  // Add a gap below
+
+      
+      //... Last content row.
+//content.add(new Gap()  , pos.nextRow().width().expandH());
+      //content.add(connectionStatus, pos.nextCol().nextCol());
+      //content.add(connectionStatus, pos.nextRow().nextCol().nextCol());
+         //... Add an area that can expand at the bottom.
+
+        //content.add(new Gap()  , pos.nextRow().width().expandH());
+
+
+
+        
+        windowFrame.add(content);
+        return content;
+   }
+
+
+   private void buildJFrameGridLayout(JPanel inputsPanel, JPanel connectPanel) {
+
+      // Setup Frame
+      GridLayout gridLayout = new GridLayout(2,1);
+      gridLayout.setVgap(20);
+
+      windowFrame.setLayout(gridLayout);
+      windowFrame.add(inputsPanel);
+      windowFrame.add(connectPanel);
+   }
+
+
+   public void updateConnectionMessage(String message) {
+      connectionStatus.setText(message);
+   }
 
    /**
     * INSTANCE VARIABLES
@@ -130,7 +222,17 @@ public class DriverScreen {
    private JTextField connectionStatus;
    private JButton connectButton;
 
+   private JButton calibrateButton;
+   private JButton configButton;
+
    private int numInputs;
+
+   // Constants
+   private static final int BORDER = 12;  // Window border in pixels.
+   private static final int GAP = 5;   // Default gap btwn components.
+
+
+
 
 
 }
---
+++ src/JavaDriver/pt/ualg/Car/JavaDriver/GUI/GridBagHelper.java
@@ -0,0 +1,114 @@
+/*
+ *  Copyright 2009 Abstract Maze.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package pt.ualg.Car.JavaDriver.GUI;
+
+// File   : layoutDemos/layoutDemoGB/GridBagHelper.java
+
+import java.awt.GridBagConstraints;
+
+// Purpose: Keeps track of current position in GridBagLayout.
+//          Supports a few GridBag features: position, width, height, expansion.
+//          All methods return GridBagHelper object for call chaining.
+// Author : Fred Swartz - January 30, 2007 - Placed in public domain.
+
+
+//////////////////////////////////////////////////////////////////// Class
+public class GridBagHelper extends GridBagConstraints {
+
+    //============================================================== constructor
+    /* Creates helper at top left, component always fills cells. */
+    public GridBagHelper() {
+        gridx = 0;
+        gridy = 0;
+        fill = GridBagConstraints.BOTH;  // Component fills area
+    }
+
+    //================================================================== nextCol
+    /* Moves the helper's cursor to the right one column. */
+    public GridBagHelper nextCol() {
+        gridx++;
+        return this;
+    }
+
+    //================================================================== nextRow
+    /* Moves the helper's cursor to first col in next row. */
+    public GridBagHelper nextRow() {
+        gridx = 0;
+        gridy++;
+        return this;
+    }
+
+    //================================================================== expandW
+    /* Expandable Width.  Returns new helper allowing horizontal expansion.
+       A new helper is created so the expansion values don't
+       pollute the origin helper. */
+    public GridBagHelper expandW() {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.weightx = 1.0;
+        return duplicate;
+    }
+
+    //================================================================== expandH
+    /* Expandable Height. Returns new helper allowing vertical expansion. */
+    public GridBagHelper expandH() {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.weighty = 1.0;
+        return duplicate;
+    }
+
+    //==================================================================== width
+    /* Sets the width of the area in terms of number of columns. */
+    public GridBagHelper width(int colsWide) {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.gridwidth = colsWide;
+        return duplicate;
+    }
+
+    //==================================================================== width
+    /* Width is set to all remaining columns of the grid. */
+    public GridBagHelper width() {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.gridwidth = REMAINDER;
+        return duplicate;
+    }
+
+    //=================================================================== height
+    /* Sets the height of the area in terms of rows. */
+    public GridBagHelper height(int rowsHigh) {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.gridheight = rowsHigh;
+        return duplicate;
+    }
+
+    //=================================================================== height
+    /* Height is set to all remaining rows. */
+    public GridBagHelper height() {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.gridheight = REMAINDER;
+        return duplicate;
+    }
+
+    //==================================================================== align
+    /* Alignment is set by parameter. */
+    public GridBagHelper align(int alignment) {
+        GridBagHelper duplicate = (GridBagHelper)this.clone();
+        duplicate.fill   = NONE;
+        duplicate.anchor = alignment;
+        return duplicate;
+    }
+}
